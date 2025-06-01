8f5398069faea0689e7cc0bbfcdaa92f9f30ee6a
Alperen Demirbas <alperendmrbs38@gmail.com>
2025-06-02 01:03:36 +0300

    github images and project finish

--- MyPortfolio/Controllers/DashboardController.cs
+++ MyPortfolio/Controllers/DashboardController.cs
@@ -8,6 +8,14 @@ namespace MyPortfolio.Controllers
         MyPortfolioContext context = new MyPortfolioContext();
         public IActionResult Index()
         {
+            // Yeteneklerin ortalaması
+            var averageSkill = context.Skills.Select(x => Convert.ToInt32(x.Value)).Average();
+            ViewBag.skill3 = Math.Round(averageSkill, 0);    // Virgülden sonra 0 basamak alır yani yuvarlar
+
+            // TodoList yapısı
+            var todoList = context.ToDoLists.OrderByDescending(x => x.Date).Take(20).ToList();
+            ViewBag.TodoList = todoList;
+
             return View();
         }
     }
--- MyPortfolio/obj/Debug/net8.0/MyPortfolio.AssemblyInfo.cs
+++ MyPortfolio/obj/Debug/net8.0/MyPortfolio.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyPortfolio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0d6bab8c8871721e66c7fefe298f8c16cc9ad627")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+91075bbd44aff7ba9de50889a5049645d2d520f4")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyPortfolio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyPortfolio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
